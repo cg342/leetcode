c8279bbb995f5edcea37953a746e3c9c33483df7
Gao, Chenxi <cgao5@bwh.harvard.edu>
2021-02-16 23:32:57 -0500

    su

--- 37_SudokuSolver.py
+++ 37_SudokuSolver.py
@@ -3,29 +3,31 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """           
-        def validate(x, y, digit):
-            for i in range(9):
-                if board[x][i] == digit or board[i][y] == digit:
+        def validate(i, j, digit):
+            for a in range(9):
+                if board[i][a] == digit or board[a][j] == digit:
                     return False
-            for i in (0,1,2):
-                for j in (0,1,2):
-                    if board[x//3*3+i][y//3*3+j] == digit:
-                        return False
+            
+            for x in (0,1,2):
+                for y in (0,1,2):
+                    if board[i-i%3+x][j-j%3+y] == digit:
+                        return False 
             return True
-                     
+            
         def solve(count):
             if count == 81:
                 return True
-            x, y = count // 9, count % 9
-            if board[x][y] != '.':
+            i, j = count//9, count%9
+            if board[i][j] != '.':
                 return solve(count+1)
-            for i in range(1, 10):
-                digit = str(i)
-                if validate(x, y, digit):
-                    board[x][y] = digit
-                    if solve(count + 1):
+            
+            for k in range(1, 10):
+                digit = str(k)
+                if validate(i, j, digit):
+                    board[i][j] = digit
+                    if solve(count+1):
                         return True
-                    board[x][y] = '.'
+                    board[i][j] = '.'
             return False
-        
+            
         solve(0)
